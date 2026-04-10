e373b2723440e59ff5061beb0bcc731674e39746
SebasLee98 <sebastianhclee@gmail.com>
2026-04-09 18:00:14 -0700

    comments

--- oews.sql
+++ oews.sql
@@ -1,5 +1,6 @@
 SELECT * FROM `oews-2009-2025_original` WHERE `Occupational Title` LIKE '%Engineer%';
 
+-- Filter only for engineers
 SELECT DISTINCT Year from `oews-2009-2025_original` WHERE `Occupational Title` LIKE '%Engineer%';
 
 SELECT DISTINCT `Occupational Title` FROM `oews-2009-2025_original`
