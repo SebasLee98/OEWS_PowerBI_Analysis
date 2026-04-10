5a1bf483132131ae9a4e8f349b45de36f2b72b13
SebasLee98 <sebastianhclee@gmail.com>
2026-04-09 17:56:22 -0700

    first commit

---
+++ oews.sql
@@ -0,0 +1,5 @@
+SELECT * FROM `oews-2009-2025_original` WHERE `Occupational Title` LIKE '%Engineer%';
+
+SELECT DISTINCT Year from `oews-2009-2025_original` WHERE `Occupational Title` LIKE '%Engineer%';
+
+SELECT DISTINCT `Occupational Title` FROM `oews-2009-2025_original`
